33da3cd97cd32c4a40fb0453bd100899bff71862
rcc <rcc>
2017-01-05 12:59:38 -0800

    Added new files and first homework
    Signed-off-by: rcc <rcc>

---
+++ Home Work/Assignment 1/Savitch_8thEd_Chap1_Prob10_BigC/main.cpp
@@ -0,0 +1,47 @@
+/* 
+  File:   main.cpp
+  Author: Victor Cuchilla
+  Created on January 5, 2017, 12:15 PM
+  Purpose:  Template to be used in all programming
+            projects!
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+//Such as PI, Vc, -> Math/Science values
+//as well as conversions from system of units to 
+//another
+
+//Function Prototypes
+
+//Executable code begins here!!!
+int main(int argc, char** argv) {
+    //Declare Variables
+    char c; //Variable to build the big C with
+    //Input values
+    cout<<"This program outputs a large c"<<endl;
+    cout<<"With a letter the user chooses"<<endl;
+    cout<<"What letter would you like?"<<endl;
+    cin>>c;
+    //Process by mapping inputs to outputs
+    
+    //Output values
+    cout<<"  "<<c<<" "<<c<<" "<<c<<endl;
+    cout<<" "<<c<<"     "<<c<<endl;
+    cout<<" "<<c<<endl;
+    cout<<" "<<c<<endl;
+    cout<<" "<<c<<endl;
+    cout<<" "<<c<<endl;
+    cout<<" "<<
+            c<<endl;
+    cout<<" "<<c<<"     "<<c<<endl;
+    cout<<"  "<<c<<" "<<c<<" "<<c<<endl;
+    //Exit stage right!
+    return 0;
+   
+}
---
+++ Home Work/README.md
@@ -0,0 +1,2 @@
+# Cuchilla_Victor_CIS5_40488_RCC
+CIS5 RCC Winter 2017
